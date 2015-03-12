eddbf9c6e55fe0e188149751b0051fba6cea68c0
Lauren Ogawa <logawa12@yahoo.com>
2015-03-11 18:29:30 -1000

    Update CalEvent.java

--- CalEvent.java
+++ CalEvent.java
@@ -8,11 +8,14 @@ import java.io.IOException;
 public class CalEvent {
 	public static void main(String[] args) {
 		File file = null;
-		//boolean bool = false;
-		String start = "BEGIN:VCALENDAR\n" +
-					"CALSCALE:GREGORIAN\n" +
-					"VERSION:2.0\n";
-		String append = "Appends to the file!";
+		
+
+		String start = "BEGIN:VCALENDAR\n";
+		String ver = "VERSION:";
+		double verNum = 2.0;
+
+		// Convert double to string
+		String strVerNum = Double.toString(verNum);
 
 		// This is where the file and text are created
 		try {
@@ -22,10 +25,12 @@ public class CalEvent {
 			// Writing to the ics file
 			// FileWriter will append to the file
 			BufferedWriter output = new BufferedWriter(new FileWriter(file));
+			
+			// Writing to the file
 			output.write(start);
-			output.write(append);
+			output.write(ver);
+			output.write(strVerNum);
 			output.close();
-
 		}
 		catch(Exception e) {
 			e.printStackTrace();
