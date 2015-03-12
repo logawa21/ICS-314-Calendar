daf94832a7196277756cc719a2f954bafa9695af
Lauren Ogawa <logawa12@yahoo.com>
2015-03-11 19:13:41 -1000

    Update CalEvent.java
    Added some of the user input prompts.
    Concat the values to make it easier to write in a value.
    So far I have the summary and location working.
    Not sure if this is the order in which it is supposed to go in the file. Does the location matter? Will check this after the rest of it is up and running.

--- CalEvent.java
+++ CalEvent.java
@@ -4,15 +4,40 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.Writer;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CalEvent {
 	public static void main(String[] args) {
 		File file = null;
 		
-
 		String start = "BEGIN:VCALENDAR\n";
 		String ver = "VERSION:";
 		double verNum = 2.0;
+		String newLine = "\n";
+		String input;
+		String summary = "SUMMARY:";
+		String location = "LOCATION:";
+		//String priority = "PRIORITY:";
+		//String class = "CLASS:";	// public, private, confidential
+		//int intPriority;			// 1-4 high, 5 meduim, 6-9 low
+		//sTime;	// Date time start; 20150508T180000
+		//eTime;	// Date time end; end comes before the start in the file
+		//timeZone;		// Time Zone
+
+		ver = ver.concat(verNum + "\n");
+		//ver = ver.concat(newLine);
+		// Get user input
+		Scanner scan = new Scanner(System.in);
+
+		// Get the name of the event
+		System.out.print("Name of the event: ");
+		input = scan.nextLine();
+		summary = summary.concat(input + "\n");
+
+		// Get the location of the event
+		System.out.print("Location of the event: ");
+		input = scan.nextLine();
+		location = location.concat(input + "\n");
 
 		// Convert double to string
 		String strVerNum = Double.toString(verNum);
@@ -29,8 +54,10 @@ public class CalEvent {
 			// Writing to the file
 			output.write(start);
 			output.write(ver);
-			output.write(strVerNum);
+			output.write(summary);
+			output.write(location);
 			output.close();
+
 		}
 		catch(Exception e) {
 			e.printStackTrace();
