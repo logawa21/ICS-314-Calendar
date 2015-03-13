6e55b80c4aecea835cefa816d7a1439edaedb84a
Lauren Ogawa <logawa12@yahoo.com>
2015-03-12 18:33:29 -1000

    Update CalEvent.java
    So I think this is my final file. I've tested it and it works.
    So it's able to take two dates and I added the timezone thing. Not sure if we need the standard one too (because of what the check in says). Let me know if you want me to put it in.

--- CalEvent.java
+++ CalEvent.java
@@ -1,33 +1,24 @@
-import java.io.File;
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.Writer;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;;
+import java.util.*;
+import java.text.*;
 
 public class CalEvent {
 	public static void main(String[] args) {
+		final String userFormat = "MM/dd/yyyy";
+		final String calFormat = "yyyy/MM/dd";
+
 		File file = null;
 		
-		String start = "BEGIN:VCALENDAR\n";
-		String ver = "VERSION:";
+		String ver = "VERSION:", pvt = "PRIVATE", pub = "PUBLIC", con = "CONFIDENTIAL";
 		double verNum = 2.0;
-		String newLine = "\n";
-		String input;
-		String summary = "SUMMARY:";
-		String location = "LOCATION:";
-		String date = "";
-		//String sTime = "DTSTART;";	// Date time start; 20150508T180000
-		//String eTime = "DTEND;";	// Date time end; end comes before the start in the file
-		//String priority = "PRIORITY:";
-		//String class = "CLASS:";	// public, private, confidential
-		//int intPriority;			// 1-4 high, 5 meduim, 6-9 low
-		//String timeZone = "";		// Time Zone
-		//String end = "END:VEVENT";
+		String tz = "", timez = "", dtstart = "", dtend = "", upper = "";
+		String location = "", summary = "", input = "", cl = "", priority = "";
+		String newStartDateStr  = "", newEndDateStr = "";
+		String[] splitTime, splitDate;
+		String sTime = "", eTime = "", sHTime = "", sMTime = "", eHTime = "", eMTime = "", sDate = "", eDate = "";
+		int intETime, intSTime, intSHTime, intSMTime, intEHTime, intEMTime, intPriority;
 
 		ver = ver.concat(verNum + "\n");
-		//ver = ver.concat(newLine);
 		// Get user input
 		Scanner scan = new Scanner(System.in);
 
@@ -42,9 +33,256 @@ public class CalEvent {
 		location = location.concat(input + "\n");
 
 		// Get the date of the event
-		System.out.print("From Date: ");
+		System.out.print("Date (MM/DD/YYYY or MM/DD/YYYY to MM/DD/YYYY): ");
 		input = scan.nextLine();
-		date = date.concat(input + "\n");
+
+		// If the event is overnight or over several days
+		if(input.contains(" to ")) {
+			splitDate = input.split(" to ");
+			sDate = splitDate[0];
+			eDate = splitDate[1];
+
+			SimpleDateFormat sdfStart = new SimpleDateFormat(userFormat);
+			SimpleDateFormat sdfEnd = new SimpleDateFormat(userFormat);
+
+			// Change the format of the date
+			try {
+				Date sd = new Date();
+				Date ed = new Date();
+
+				// Change the start date format
+				sd = sdfStart.parse(sDate);
+				
+				// Changing the format
+				sdfStart.applyPattern(calFormat);
+				newStartDateStr = sdfStart.format(sd);
+
+				// Fixed the format, but not showing up in iCal
+				// Change the end date format
+				ed = sdfEnd.parse(eDate);
+				sdfEnd.applyPattern(calFormat);
+				newEndDateStr = sdfEnd.format(ed);
+			}
+			catch(ParseException pe) {
+				pe.printStackTrace();
+			}
+			// Delete the "/" in the start date
+			sDate = newStartDateStr.replaceAll("/", "");
+			dtstart = dtstart.concat(sDate);
+
+			// Delete the "/" in the end date
+			eDate = newEndDateStr.replaceAll("/", "");
+			dtend = dtend.concat(eDate);
+		}
+		else {
+			// If the event is only one day
+			// Change the date format from MM/dd/yyyy to yyyy/MM/dd
+			SimpleDateFormat sdf = new SimpleDateFormat(userFormat);
+			try {
+				Date d = new Date();
+				d = sdf.parse(input);
+				// Changing the format
+				sdf.applyPattern(calFormat);
+				newStartDateStr = sdf.format(d);
+			}
+			catch(ParseException pe) {
+				pe.printStackTrace();
+			}
+
+			// Delete the "/"
+			input = newStartDateStr.replaceAll("/", "");
+			dtstart = dtstart.concat(input);
+
+			// For now assuming we are only adding an event on one day
+			dtend = dtstart;
+		}
+
+		// Get the time of the event
+		System.out.print("Time (6am-8pm): ");
+		input = scan.nextLine();
+
+		// Split the times into start time and end time while getting rid of "-"
+		if(input.contains("-")) {
+			splitTime = input.split("-");
+			sTime = splitTime[0];
+			eTime = splitTime[1];
+		}
+		// Convert the start time
+		if(sTime.contains("pm")) {
+			// Get rid of pm
+			sTime = sTime.replace("pm", "");
+
+			// Split the hours and minutes
+			if(sTime.contains(":")) {
+				splitTime = sTime.split(":");
+				sHTime = splitTime[0];
+				sMTime = splitTime[1];
+
+				// Change it into an int to convert it
+				intSHTime = Integer.parseInt(sHTime);
+				intSHTime = intSHTime + 12;
+				// Add it to dtstart
+				dtstart = dtstart.concat("T" + intSHTime + sMTime + "00\n");
+			}
+			else {
+				// Change it into an int to convert it
+				intSTime = Integer.parseInt(sTime);
+				intSTime = intSTime + 12;
+				// Add it to dtstart
+				dtstart = dtstart.concat("T" + sTime + "00\n");
+			}
+		}
+		else if (sTime.contains("am")) {
+			// Get rid of pm
+			sTime = sTime.replace("am", "");
+
+			// Split the hours and minutes
+			if(sTime.contains(":")) {
+				splitTime = sTime.split(":");
+				sHTime = splitTime[0];
+				sMTime = splitTime[1];
+
+				// Add it to dtstart
+				dtstart = dtstart.concat("T" + sHTime + sMTime + "00\n");
+			}
+			else {
+				intSTime = Integer.parseInt(sTime);
+				intSTime = intSTime + 12;
+				dtstart = dtstart.concat("T" + intSTime + "00\n");
+			}
+		}
+		else if (sTime.contains(":")){
+			splitTime = sTime.split(":");
+			sHTime = splitTime[0];
+			sMTime = splitTime[1];
+
+			// Change it into an int to convert it
+			intSHTime = Integer.parseInt(sHTime);
+			intSHTime = intSHTime + 12;
+			// Add it to dtstart
+			dtstart = dtstart.concat("T" + intSHTime + sMTime + "00\n");
+		}
+		else {
+			intSHTime = Integer.parseInt(sTime);
+			if (intSHTime <= 9) {
+				sTime = "0" + sTime;
+				dtstart = dtstart.concat("T" + sTime + "0000\n");
+			}
+			else {
+				dtstart = dtstart.concat("T" + sTime + "0000\n");
+			}
+		}
+
+		// Convert the end time
+		if(eTime.contains("pm")) {
+			// Get rid of pm
+			eTime = eTime.replace("pm", "");
+
+			// Split the hours and minutes
+			if(eTime.contains(":")) {
+				splitTime = eTime.split(":");
+				eHTime = splitTime[0];
+				eMTime = splitTime[1];
+
+				// Change it into an int to convert it
+				intEHTime = Integer.parseInt(eHTime);
+				intEHTime = intEHTime + 12;
+
+				// Add it to dtend
+				dtend = dtend.concat("T" + intEHTime + eMTime + "00\n");
+			}
+			else {
+				// Change it into an int to convert it
+				intETime = Integer.parseInt(eTime);
+				intETime = intETime + 12;
+
+				// Add it to dtend
+				dtend = dtend.concat("T" + intETime + "0000\n");
+			}
+		}
+		else if (eTime.contains("am")) {
+			// Get rid of pm
+			eTime = eTime.replace("am", "");
+
+			// Split the hours and minutes
+			if(eTime.contains(":")) {
+				splitTime = eTime.split(":");
+				eHTime = splitTime[0];
+				eMTime = splitTime[1];
+
+				// Change it into an int to convert it
+				intEHTime = Integer.parseInt(eHTime);
+				intEHTime = intEHTime + 12;
+				// Add it to dtstart
+				dtend = dtend.concat("T" + intEHTime + eMTime + "00\n");
+			}
+			else {
+				intETime = Integer.parseInt(eTime);
+				intETime = intETime + 12;
+				dtend = dtend.concat("T" + eTime + "0000\n");
+			}
+		}
+		else if (eTime.contains(":")){
+			splitTime = eTime.split(":");
+			eHTime = splitTime[0];
+			eMTime = splitTime[1];
+
+			// Change it into an int to convert it
+			intEHTime = Integer.parseInt(eHTime);
+			intEHTime = intEHTime + 12;
+			// Add it to dtstart
+			dtend = dtend.concat("T" + intEHTime + eMTime + "00\n");
+		}
+		else {
+			intEHTime = Integer.parseInt(eTime);
+			if (intEHTime <= 9) {
+				eTime = "0" + eTime;
+				dtend = dtend.concat("T" + eTime + "0000\n");
+			}
+			else {
+				dtend = dtend.concat("T" + eTime + "0000\n");
+			}
+		}
+
+		// Get the timezone of the event
+		System.out.print("Timezone (HST): ");
+		input = scan.nextLine();
+		timez = input;
+		if(input.contains("HST")) {
+			tz = tz.concat("TZID=Pacific/Honolulu");
+		}
+		else {
+			// Force it to be PST
+			tz = tz.concat("TZID=Pacific/Honolulu");
+		}
+
+		// Get the classification of the event
+		System.out.print("Classification (public, private, or confidential: ");
+		input = scan.nextLine();
+
+		upper = input.toUpperCase();
+		// Write the classification depending on what they wrote
+		if(upper == pvt) {
+			cl = cl.concat(pvt + "\n");
+		}
+		else if(input == con) {
+			cl = cl.concat(con + "\n");
+		}
+		else {
+			cl = cl.concat(pub + "\n");
+		}
+
+		// Get the priority of the event
+		System.out.print("Priority of the event (0-9): ");
+		intPriority = scan.nextInt();
+		if(intPriority == 0)
+			priority = priority.concat("UNDEFINED" + "\n");
+		if(intPriority >= 1 && intPriority <= 4)
+			priority = priority.concat("HIGH" + "\n");
+		if(intPriority == 5)
+			priority = priority.concat("MEDIUM" + "\n");
+		if(intPriority >= 6 && intPriority <= 9)
+			priority = priority.concat("LOW" + "\n");
 
 		// This is where the file and text are created
 		try {
@@ -56,10 +294,19 @@ public class CalEvent {
 			BufferedWriter output = new BufferedWriter(new FileWriter(file));
 			
 			// Writing to the file
-			output.write(start);
+			output.write("BEGIN:VCALENDAR\n");
 			output.write(ver);
-			output.write(summary);
-			output.write(location);
+
+			output.write("BEGIN:VEVENT\n");
+			output.write("DTSTART;" + tz + ":" + dtstart);
+			output.write("DTEND;" + tz + ":" + dtend);
+			output.write("LOCATION:" + location);
+			output.write("SUMMARY:" + summary);
+			output.write("CLASS:" + cl);
+			output.write("PRIORITY:" + priority);
+			output.write("END:VEVENT\n");
+
+			output.write("END:VCALENDAR");
 			output.close();
 
 		}
